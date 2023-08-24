9f6c79306d3f3f6211ad4d0994a535c36c6d26ca
Artem Gorodko <gorodkoartem00@gmail.com>
2023-08-24 21:20:09 +0300

    Added HW from Lesson1 (additional task: load an assembly dinamically)

---
+++ Lesson1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var asm = Assembly.LoadFrom("ConsoleApp2.dll");
+            var program = asm.GetType("ConsoleApp2.Program");
+
+            var main = program.GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static);
+            main.Invoke(null, new object[] { new string[] { } });
+
+            var sayHello = program.GetMethod("SayHello", BindingFlags.NonPublic | BindingFlags.Static);
+            sayHello.Invoke(null, null);
+        }
+    }
+}
---
+++ Lesson1/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{nameof(Main)} method from {GetAssemblyName()}");
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine($"{nameof(SayHello)} method from {GetAssemblyName()}");
+        }
+
+        private static string GetAssemblyName()
+        {
+            var assembly = Assembly.GetAssembly(typeof(Program));
+            return assembly.GetName().Name;
+        }
+    }
+}
