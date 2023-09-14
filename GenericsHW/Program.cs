c0d8c08bea97c37f508a8a86cecffbbb6e4652fa
Artem Gorodko <gorodkoartem00@gmail.com>
2023-09-14 17:54:38 +0300

    Added Generics HW (lesson 11)

---
+++ GenericsHW/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericsHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            stack.Pop();
+
+            stack.Push(4);
+
+
+
+            var element = stack.Peek();
+
+            if(!stack.IsEmpty)
+            {
+                element = stack.Pop();
+            }
+
+        }
+    }
+}
---
+++ GenericsHW/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+
+namespace GenericsHW
+{
+    public class Stack<T>
+    {
+        private const int DefaultSize = 2;
+        private T[] _values = new T[DefaultSize];
+        private int _currentLength;
+
+        public bool IsEmpty => _currentLength == 0;
+
+        public void Push(T value)
+        {
+            if(_values.Length <=  _currentLength)
+            {
+                IncreaseArray();
+            }
+
+            _values[_currentLength++] = value;
+        }
+
+        public T Pop()
+        {
+            if (_currentLength == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            var result = _values[_currentLength - 1];
+            _values[_currentLength - 1] = default;
+            _currentLength--;
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (_currentLength == 0)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return _values[_currentLength - 1];
+        }
+
+        private void IncreaseArray()
+        {
+            var newArray = new T[_values.Length * 2];
+            Array.Copy(_values, newArray, _values.Length);
+            _values = newArray;
+        }
+    }
+}
