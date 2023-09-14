6023aa452d6017f7511709621d752666d063e266
Artem Gorodko <gorodkoartem00@gmail.com>
2023-09-14 17:57:06 +0300

    Added solution from lesson 11

---
+++ Lesson11 (solution from the lesson)/MyList.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+
+namespace Sandbox
+{
+    class MyListItem<T>
+    {
+        public T Value { get; set; }
+        public MyListItem<T> Next { get; set; }
+    }
+
+    interface IMyList<T> : IEnumerable<T>
+    {
+        void Add(T param);
+    }
+
+    // 1 => 3 => 2 => 4
+
+    internal class MyList<T> : IMyList<T>
+    {
+        private MyListItem<T> head;
+
+        public T this[int index]
+        {
+            get
+            {
+                var currentItem = head;
+                for (var i = 0; i < index; i++)
+                {
+                    if(currentItem == null)
+                        throw new IndexOutOfRangeException();
+
+                    currentItem = currentItem.Next;
+                }
+
+                return currentItem.Value;
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (head == null)
+            {
+                head = new MyListItem<T> { Value = value };
+                return;
+            }
+
+            head = new MyListItem<T> { Value = value, Next = head };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyEnymerator<T>(head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class MyEnymerator<T> : IEnumerator<T>
+    {
+        private readonly MyListItem<T> head;
+        private MyListItem<T> current;
+
+        public MyEnymerator(MyListItem<T> head)
+        {
+            this.head = head;
+        }
+
+        public T Current => current.Value;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if(current == null)
+            {
+                current = head;
+                return head != null;
+            }
+
+            if(current.Next == null)
+                return false;
+
+            current = current.Next;
+            return true;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
---
+++ Lesson11 (solution from the lesson)/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Sandbox
+{
+    class BaseClass
+    {
+        public string Name1 { get; set; }
+    }
+
+    class MyClass : BaseClass
+    {
+        public string Name2 { get; set; }
+    }
+
+    class DerivedClass : MyClass
+    {
+        public string Name3 { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+
+            new Stack<int>().Add(1);
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static BaseClass Method1(DerivedClass param)
+        {
+            var a = param.Name3;
+            return new BaseClass();
+        }
+
+        static BaseClass Method2(MyClass param)
+        {
+            return new BaseClass(); 
+        }
+
+        static MyClass Method3(BaseClass param)
+        {
+            return new MyClass();
+        }
+    }
+}
