4c5031335995d2b342cc995f8cfbfab63796ccbe
Artem Gorodko <gorodkoartem00@gmail.com>
2023-09-15 08:40:23 +0300

    Added solution from the lesson 14

---
+++ Lesson14 (solution from the lesson)/SimpleHttpServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace SimpleHttpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            listener.Prefixes.Add("http://127.0.0.1:8888/");
+            listener.Start();
+            Console.WriteLine("Listening...");
+            // Note: The GetContext method blocks while waiting for a request.
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+        }
+    }
+}
