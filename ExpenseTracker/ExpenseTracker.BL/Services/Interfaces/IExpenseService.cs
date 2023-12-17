0694b857b73d0cd30e2ae3b639b17c5892c328b9
Artem Gorodko <gorodkoartem00@gmail.com>
2023-12-17 22:09:36 +0300

    Updated IExpenseService interface

--- ExpenseTracker/ExpenseTracker.BL/Services/Interfaces/IExpenseService.cs
+++ ExpenseTracker/ExpenseTracker.BL/Services/Interfaces/IExpenseService.cs
@@ -4,9 +4,9 @@ namespace ExpenseTracker.BL.Services.Interfaces;
 
 public interface IExpenseService
 {
-    ServiceDataResponse<IEnumerable<Expense>> GetExpenses(Category? category, Account? account);
-    ServiceDataResponse<Expense> GetExpenseById(Guid id);
-    ServiceDataResponse<Guid> CreateExpense(Expense expense);
-    ServiceDataResponse<Expense> UpdateExpense(Expense expense);
-    ServiceResponse DeleteExpense(Expense expense);
+    Task<ServiceDataResponse<IEnumerable<Expense>>> GetExpensesAsync(Category? category, Account? account);
+    Task<ServiceDataResponse<Expense>> GetExpenseByIdAsync(Guid id);
+    Task<ServiceDataResponse<Expense>> CreateExpenseAsync(Expense expense);
+    Task<ServiceDataResponse<Expense>> UpdateExpenseAsync(Expense expense);
+    Task<ServiceResponse> DeleteExpenseAsync(Expense expense);
 }
